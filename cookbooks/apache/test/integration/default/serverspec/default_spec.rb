ef6dc35279217ad1cfc1d0dd4973ec389aac4a36
Michel Richard <mrichard4@LM3000026417.local>
2014-12-04 15:25:38 -0500

    test

---
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,19 @@
+require 'serverspec'
+set :backend, :exec
+
+describe 'apache' do
+
+	it "is awesome" do
+		expect(true).to eq true
+	end
+
+	it "is installed"
+
+	it "is running"
+
+	it "is responding to http request"
+
+	it "is displaying the proper home page"
+
+	it "is running on the default port"
+end
