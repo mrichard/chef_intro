9f0505826576058d963ccf111d145d8f9e67c982
Michel Richard <mike_r1978_6@hotmail.com>
2014-12-04 17:26:45 +0000

    add motd recipe

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+file "/etc/motd" do
+	action :create
+	content "Property of SAKS"
+	mode "0644"
+	owner "root"
+	group "root"
+end
